2b92bc3f1761434bd3ae1f76c931dbcb61c19cd4
zimo ding <dingzm98@tamu.edu>
2019-11-11 17:13:00 -0600

    11

--- search.js
+++ search.js
@@ -3,9 +3,10 @@ function search(){
         url: "search.json",     //this is one of many REST endpoints that are available
         method: "GET",
         data: {    //use the data object literal to specify parameters, as documented in the REST API section of this developer site
-          "search": "Natural",
+          "search": "Howdy",
           "page": "1",
           "num_per_page":"3",
+          "search_group":"9734488064",
         },
         success: function (search) { //print message response information to the console
           alert("The request was successful.");
