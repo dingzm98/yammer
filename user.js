a33f2a51581584fa2cc536c949ec3acf6a636707
zimo ding <dingzm98@tamu.edu>
2019-11-11 16:43:18 -0600

    a

--- user.js
+++ user.js
@@ -1,6 +1,6 @@
 function user(){
     yam.platform.request({
-        url: "users.json",     //this is one of many REST endpoints that are available
+        url: "users/current.json",     //this is one of many REST endpoints that are available
         method: "GET",
         data: {    //use the data object literal to specify parameters, as documented in the REST API section of this developer site
           "letter": "a",
