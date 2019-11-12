cb3fcd8b58d80680d6da28b84a64c89ad37498a9
dingzm98 <dingzm98@tamu.edu>
2019-11-12 17:09:00 -0600

    1

--- search.js
+++ search.js
@@ -12,7 +12,11 @@ function search(){
         success: function (search) { //print message response information to the console
           alert("The request was successful.");
           console.dir(search);
-          console.log(search.messages);
+          var i;
+          for (i = 0; i < search.count.messages; i++) {
+            console.log(search.messages.messages[i].content_excerpt);
+          }
+          
         },
         error: function (search) {
           alert("There was an error with the request.");
