8ca548ada28f6a5dc07baa5ad0cff11007c9c6c2
dingzm98 <dingzm98@tamu.edu>
2019-11-11 23:00:33 -0600

    1

---
+++ current_user.js
@@ -0,0 +1,16 @@
+function current_user(){
+    yam.platform.request({
+        url: "users/current.json",     //this is one of many REST endpoints that are available
+        method: "GET",
+        data: {    //use the data object literal to specify parameters, as documented in the REST API section of this developer site
+          "page": "2",
+        },
+        success: function (user) { //print message response information to the console
+          alert("The request was successful.");
+          console.dir(user);
+        },
+        error: function (user) {
+          alert("There was an error with the request.");
+        }
+      });
+}
