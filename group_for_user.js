8b26e043c54f333892216353a77e420822c019f8
dingzm98 <dingzm98@tamu.edu>
2019-11-12 16:12:34 -0600

    1

--- group_for_user.js
+++ group_for_user.js
@@ -1,10 +1,10 @@
 function group_for_user(){
+  var useridg = document.getElementById('userid').value;
     yam.platform.request({
-        url: "groups/for_user/:User_Id.json.json",     //this is one of many REST endpoints that are available
+        url: "groups/for_user/"+useridg+".json",     //this is one of many REST endpoints that are available
         method: "GET",
         data: {    //use the data object literal to specify parameters, as documented in the REST API section of this developer site
-          "letter": "a",
-          "page": "2",
+          "User_Id": useridg,
         },
         success: function (user) { //print message response information to the console
           alert("The request was successful.");
