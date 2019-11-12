5a438d7ee3fc7e473bb25d46f9ddd10474a27c07
dingzm98 <dingzm98@tamu.edu>
2019-11-11 20:53:21 -0600

    1

--- search.js
+++ search.js
@@ -1,9 +1,10 @@
 function search(){
+  var search1 = document.getElementById('search').value;
     yam.platform.request({
         url: "search.json",     //this is one of many REST endpoints that are available
         method: "GET",
         data: {    //use the data object literal to specify parameters, as documented in the REST API section of this developer site
-          "search": "Howdy",
+          "search": search1,
           "page": "1",
           "num_per_page":"4",
           "search_group":"9734488064",
