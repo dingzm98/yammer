a0b2712bb6261dd118f6a67494ccd40bf6c78a1a
zimo ding <dingzm98@tamu.edu>
2019-11-12 21:00:48 -0600

    1

---
+++ search_by_email.js
@@ -0,0 +1,21 @@
+function search_by_email(){
+  var email = document.getElementById('email').value;
+    yam.platform.request({
+        url: "/users/by_email.json?email="+email,     //this is one of many REST endpoints that are available
+        method: "GET",
+        data: {    //use the data object literal to specify parameters, as documented in the REST API section of this developer site
+          "User_Email": email,
+        },
+        success: function (user) { //print message response information to the console
+          alert("The request was successful.");
+          console.dir(user);
+          var i;
+          for (i = 0; i < user.length; i++) {
+            console.log(user[i].full_name);
+          }
+        },
+        error: function (user) {
+          alert("There was an error with the request.");
+        }
+      });
+}
