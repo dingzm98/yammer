949c815376d6ae9e59e9c58bf174e10d98b9e6b8
zimo ding <dingzm98@tamu.edu>
2019-11-21 14:34:57 -0600

    finish

---
+++ likemessage.js
@@ -0,0 +1,16 @@
+function likemessage(){
+  var messageid = document.getElementById('likemessage').value;
+    yam.platform.request({
+        url: "messages/liked_by/current.json?message_id="+messages,     //this is one of many REST endpoints that are available
+        method: "POST",
+        data: {    //use the data object literal to specify parameters, as documented in the REST API section of this developer site
+          "message_id": messageid,
+        },
+        success: function (user) { //print message response information to the console
+          alert("The request was successful.");
+        },
+        error: function (user) {
+          alert("There was an error with the request.");
+        }
+      });
+}
