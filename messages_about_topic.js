f973ff762caae2978a09a5aef784e63f153e45d6
zimo ding <dingzm98@tamu.edu>
2019-11-21 11:27:08 -0600

    123

--- messages_about_topic.js
+++ messages_about_topic.js
@@ -10,8 +10,8 @@ function messages_about_topic(){
           alert("The request was successful.");
           console.dir(user);
           var i;
-          for (i = 0; i < user.users.length; i++) {
-            console.log(user.users[i].full_name);
+          for (i = 0; i < user.messages.length; i++) {
+            console.log(user.messages[i].content_excerpt);
           }
         },
         error: function (user) {
