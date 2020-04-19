991d8ea91a548be874b0b65c88e793104993e2fc
dingzm98 <dingzm98@tamu.edu>
2020-04-18 21:59:04 -0500

    Update sendmessage.js

--- sendmessage.js
+++ sendmessage.js
@@ -1,15 +1,30 @@
 function sendmessage(){
   var groupid = document.getElementById('sendgroup').value;
   var content = document.getElementById('sendcontent').value;
+  var tag;
+  let req = new XMLHttpRequest();
+        req.open('POST', 'http://a089ec8b4801511eaaf7502ed822b4dd-589797411.us-west-2.elb.amazonaws.com/mml-test-api');
+        req.setRequestHeader('Content-Type', 'application/json');
+        req.onload = function() {
+            let response = JSON.parse(req.responseText);
+            console.log('success');
+            tag = response;
+        }
+        let sendData = JSON.stringify(
+            {
+                "body": content
+              }
+            );
+        req.send(sendData);
 
-  
+
     yam.platform.request({
         url: "messages.json",     //this is one of many REST endpoints that are available
         method: "POST",
         data: {    //use the data object literal to specify parameters, as documented in the REST API section of this developer site
           "body": content,
           "group_id": groupid,
-          "topic1": "tag",
+          "topic1": tag,
         },
         success: function (user) { //print message response information to the console
           alert("The request was successful.");
@@ -20,5 +35,6 @@ function sendmessage(){
       });
 
 
-
+      
+        
 }
