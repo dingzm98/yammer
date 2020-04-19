a77d38dca042b5fa67362b41495c28e2cd0adbbc
dingzm98 <dingzm98@tamu.edu>
2020-04-19 01:28:16 -0500

    Update sendmessage.js

--- sendmessage.js
+++ sendmessage.js
@@ -19,21 +19,21 @@ function sendmessage(){
         req.send(sendData);
 
 
-    yam.platform.request({
-        url: "messages.json",     //this is one of many REST endpoints that are available
-        method: "POST",
-        data: {    //use the data object literal to specify parameters, as documented in the REST API section of this developer site
-          "body": content,
-          "group_id": groupid,
-          "topic1": tag,
-        },
-        success: function (user) { //print message response information to the console
-          alert("The request was successful.");
-        },
-        error: function (user) {
-          alert("There was an error with the request.");
-        }
-      });
+    // yam.platform.request({
+    //     url: "messages.json",     //this is one of many REST endpoints that are available
+    //     method: "POST",
+    //     data: {    //use the data object literal to specify parameters, as documented in the REST API section of this developer site
+    //       "body": content,
+    //       "group_id": groupid,
+    //       "topic1": tag,
+    //     },
+    //     success: function (user) { //print message response information to the console
+    //       alert("The request was successful.");
+    //     },
+    //     error: function (user) {
+    //       alert("There was an error with the request.");
+    //     }
+    //   });
 
 
       
