0905f1e2fa1b6c514761612a04eda5ae28c34495
dingzm98 <dingzm98@tamu.edu>
2020-04-19 16:33:57 -0500

    Update sendmessage.js

--- sendmessage.js
+++ sendmessage.js
@@ -1,6 +1,23 @@
 function sendmessage(){
   var groupid = document.getElementById('sendgroup').value;
   var content = document.getElementById('sendcontent').value;
+  function sendyam(groupin,contentin,tagin){
+    yam.platform.request({
+      url: "messages.json",     //this is one of many REST endpoints that are available
+      method: "POST",
+      data: {    //use the data object literal to specify parameters, as documented in the REST API section of this developer site
+        "body": contentin,
+        "group_id": groupin,
+        "topic1": tagin
+      },
+      success: function (user) { //print message response information to the console
+        alert("The request was successful.");
+      },
+      error: function (user) {
+        alert("There was an error with the request.");
+      }
+    });
+   }
   var tag = "tag";
   let req = new XMLHttpRequest();
         req.open('POST', 'https://cors-anywhere.herokuapp.com/http://a089ec8b4801511eaaf7502ed822b4dd-589797411.us-west-2.elb.amazonaws.com/mml-test-api');
@@ -17,23 +34,9 @@ function sendmessage(){
               }
             );
         req.send(sendData);
+      sendyam(groupid,content,tag);
 
-    console.log(tag);
-    yam.platform.request({
-        url: "messages.json",     //this is one of many REST endpoints that are available
-        method: "POST",
-        data: {    //use the data object literal to specify parameters, as documented in the REST API section of this developer site
-          "body": content,
-          "group_id": groupid,
-          "topic1": tag
-        },
-        success: function (user) { //print message response information to the console
-          alert("The request was successful.");
-        },
-        error: function (user) {
-          alert("There was an error with the request.");
-        }
-      });
+        
 
 
       
